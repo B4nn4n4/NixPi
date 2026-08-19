320f7317f0fe2ad8cb61404de37c8c9e022194b5
B4nn4n4 <git@B4nn4n4.dev>
2026-08-20 00:26:32 +0200

    trust cachix substituter/keys declaratively to silence flake config prompts

--- modules/common.nix
+++ modules/common.nix
@@ -22,6 +22,11 @@
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "@wheel" ];
+      # Avoid interactive allow/trust prompts from the flake's nixConfig.
+      trusted-public-keys = [
+        "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+      ];
+      substituters = [ "https://nixos-raspberrypi.cachix.org" ];
     };
   };
 
